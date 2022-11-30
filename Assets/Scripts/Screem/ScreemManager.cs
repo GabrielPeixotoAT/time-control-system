40fd62c9e8d58d56f94271438a62e4e1a3d29816
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-11-30 18:57:44 -0300

    feat: menu inicial

--- Assets/Scripts/Screem/ScreemManager.cs
+++ Assets/Scripts/Screem/ScreemManager.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class ScreemManager : MonoBehaviour
 {
+    public GameObject mainScreem, timeScreem;
+
     void Start()
     {
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    public void BackToMain()
+    {
+        mainScreem.SetActive(true);
+        timeScreem.SetActive(false);
+    }
+
+    public void TimeScreem()
+    {
+        timeScreem.SetActive(true);
+        mainScreem.SetActive(false);
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
     }
 }
